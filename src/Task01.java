7b22585759fbd2ac1cc15f0e6f2bedc490bf6461
Artem Privalov <rashen95@mail.ru>
2022-12-04 21:48:18 +0300

    Решена первая задача

---
+++ src/Task01.java
@@ -0,0 +1,20 @@
+//Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список.
+
+import java.util.LinkedList;
+
+public class Task01 {
+    public static void main(String[] args) {
+        LinkedList<Integer> myLinkedList = new LinkedList<>();
+        for (int i = 1; i <= 10; i++) {
+            myLinkedList.add(i);
+        }
+        System.out.println("Первоначальный список:");
+        System.out.println(myLinkedList);
+        System.out.println("'Перевернутый' список:");
+        for (int i = 1; i < myLinkedList.size(); i++) {
+            myLinkedList.addLast(myLinkedList.get(myLinkedList.size()-1-i));
+            myLinkedList.remove(myLinkedList.size()-2-i);
+        }
+        System.out.println(myLinkedList);
+    }
+}
